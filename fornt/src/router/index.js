5da42d34bd56b35249f95c6f9850df918cf3b67c
luckyjolly <chengqilinjtau@sina.com>
2020-11-15 21:28:22 +0800

    页面绘制

--- fornt/src/router/index.js
+++ fornt/src/router/index.js
@@ -1,22 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Login = () => import(/* webpackChunkName: 'login' */ '../views/Login.vue')
+const Forget = () => import(/* webpackChunkName: 'forget' */ '../views/Forget.vue')
+const Reg = () => import(/* webpackChunkName: 'reg' */ '../views/Reg.vue')
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    redirect: '/login'
+  }, {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }, {
+    path: '/forget',
+    name: 'forget',
+    component: Forget
+  }, {
+    path: '/reg',
+    name: 'reg',
+    component: Reg
   }
 ]
 
---
+++ fornt/src/views/Login.vue
@@ -0,0 +1,125 @@
+<template>
+  <div id="app">
+    <div class="layui-container">
+      <form action class="layui-form layui-form-pane">
+        <div class="layui-form-item">
+          <label for class="layui-form-label">用户名</label>
+          <div class="layui-input-inline">
+            <input
+              type="text"
+              name="name"
+              placeholder="请输入标题"
+              class="layui-input"
+              v-model="name"
+              v-validate="'required|email'"
+            />
+          </div>
+          <div class="layui-form-mid error">{{errors.first('name')}}</div>
+        </div>
+        <div class="layui-form-item">
+          <label for class="layui-form-label">密码</label>
+          <div class="layui-input-inline">
+            <input
+              type="password"
+              name="password"
+              placeholder="请输入标题"
+              class="layui-input"
+              v-model="password"
+              v-validate="'required|min:6'"
+            />
+          </div>
+          <div class="layui-form-mid error">{{errors.first('password')}}</div>
+        </div>
+        <div class="layui-form-item">
+          <label for class="layui-form-label">验证码</label>
+          <div class="layui-input-inline">
+            <input
+              type="text"
+              name="code"
+              placeholder="请输入标题"
+              class="layui-input"
+              v-model="code"
+              v-validate="'required|length:4'"
+            />
+          </div>
+          <div class="layui-form-mid svg" v-html="svg" @click="getCaptcha"></div>
+          <div class="layui-form-mid error">{{errors.first('code')}}</div>
+        </div>
+        <button class="layui-btn" @click="checkForm">点击登录</button>
+        <a href="" class="imook-link">忘记密码</a>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data () {
+    return {
+      svg: '',
+      name: '',
+      password: '',
+      code: ''
+    }
+  },
+  mounted () {
+    this.getCaptcha()
+  },
+  methods: {
+    getCaptcha () {
+      axios.get('http://localhost:3000/getCaptcha').then(res => {
+        if (res.status === 200) {
+          const obj = res.data
+
+          if (obj.code === 200) {
+            this.svg = obj.data
+          }
+        }
+      })
+    },
+
+    checkForm () {
+      this.errorMsg = []
+
+      if (!this.name) {
+        this.errorMsg.push('登录名为空')
+      }
+      if (!this.password) {
+        this.errorMsg.psuh('密码不能为空')
+      }
+      if (!this.code) {
+        this.errorMsg.push('验证码为空')
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app{
+  background: #f5f5f5;
+}
+.layui-container{
+  background: #fff;
+}
+input{
+  width: 190px;
+}
+.imook-link{
+  margin-left: 10px;
+  &:hover{
+    color: #009688;
+  }
+}
+
+.svg {
+  position: relative;
+  top: -15px;
+}
+
+.error {
+  color: red;
+}
+</style>
