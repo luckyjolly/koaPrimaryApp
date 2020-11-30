2ffa2388114468e2cf0a3cc330992c5f88624201
luckyjolly <chengqilinjtau@sina.com>
2020-11-30 21:42:07 +0800

    redis 方法

--- src/config/RedisConfig.js
+++ src/config/RedisConfig.js
@@ -1,11 +1,12 @@
 import redis from 'redis'
-import { promisifyAll } from 'bluebird'
+import { promisify, promisifyAll } from 'bluebird'
 import config from './index'
+import { cli } from 'webpack';
 
 const options = {
-  host: config.redis.host,
-  port: config.redis.port,
-  password: config.redis.password,
+  host: config.REDIS.host,
+  port: config.REDIS.port,
+  password: config.REDIS.password,
   detect_buffer: true,
   retry_startegy: function (options) {
     if (options.error && options.error.code === 'ECONNREFUSED') {
@@ -19,6 +20,62 @@ const options = {
       return undefined;
     }
     // reconnect after
-    return Math.min(options.attempt * 100, 3000); // 返回数子，代表在多少毫秒后重启
+    return Math.min(options.attempt * 100, 3000); // 返回数字，代表在多少毫秒后尝试
   }
+}
+
+const client = promisifyAll(redis.createClient(options))
+
+client.on('error', err => {
+  console.log('Redis Client Error: ', err)
+})
+
+const setValue = (key, value, time) => {
+  if (!value && value !== 0) {
+    return;
+  }
+  if (typeof value === 'object') {// 存为hash
+    Object.keys(value).forEach(item => {
+
+      // hash的结构：key,field,value,以下是将hash表key中的域field设置为value[item]
+      client.hset(key, item, value[item], redis.print) // redis.print打印redis返回的日志
+    })
+  } else {
+    if (time) {
+      client.set(key, value, 'EX', time) // 设置过期时间
+    } else {
+      client.set(key, value)
+    }
+  }
+}
+
+// node >= v8
+// const { promisify } = require('util')
+// const getAsync = promisify(client.get).bind(client)
+
+const getValue = key => {
+  return client.getAsync(key)
+}
+
+const getHValue = key => {
+
+  // return promisify(client.hgetall).bind(client)(key)
+  return client.hgetallAsync(key)
+}
+
+const delValue = key => {
+  client.del(key, (err, res) => {
+    if (res === 1) {
+      console.log('delete successful')
+    } else {
+      console.log('delete redis key error:' + err)
+    }
+  })
+}
+
+export {
+  setValue,
+  getValue,
+  getHValue,
+  delValue
 }
--- src/config/test.js
+++ src/config/test.js
@@ -1,3 +1,17 @@
-import mongoose from './DBhelper'
+import { getValue, setValue, getHValue, delValue } from './RedisConfig'
 
-// mongoose.
+// setValue('imooc', 'imooc message from redis client')
+
+// getValue('imooc').then((res) => {
+//   console.log('getValue:' + res)
+// })
+
+// delValue('imooc')
+
+setValue('imoocobj', {name: 'brian', age: 30, email: 'brian@toimc.com'})
+
+// getHValue('imoocobj').then((res) => {
+//   console.log('getHValue:' +JSON.stringify(res, null, 2))
+// })
+
+// delValue('imoocobj')
